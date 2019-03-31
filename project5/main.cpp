7e8dbf254d792680615ef03c86367de8a0a13a2c
shreya chatterjee <schatterjee22@ucla.edu>
2019-03-30 22:11:37 -0700

    Create main.cpp

---
+++ project5/main.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Project5
+//
+//  Created by Shreya Chatterjee on 2/22/19.
+//  Copyright © 2019 Shreya Chatterjee. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <cassert>
+
+#include "BoxOffice.h"
+
+using namespace std;
+
+int main()
+{
+
+    
+    cout << "all tests passed!" << endl ;
+    
+    return 0;
+    
+}
+
