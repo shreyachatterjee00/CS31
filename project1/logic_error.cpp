a04b66daf13cf0b0333c72022e9cb46c75dbbcba
shreya chatterjee <schatterjee22@ucla.edu>
2019-03-30 22:01:07 -0700

    Create logic_error.cpp

---
+++ project1/logic_error.cpp
@@ -0,0 +1,53 @@
+//
+//  logic_error.cpp
+//  Project1Step6
+//
+//  Created by Shreya Chatterjee on 1/9/19.
+//  Copyright © 2019 Shreya Chatterjee. All rights reserved.
+//
+
+// Code for Project 1
+// Congress results
+
+#include <iostream>
+using namespace std;   // pp. 37-38 in Savitch book explain this line
+
+int main()
+{
+    int republicanSeats;
+    int democraticSeats;
+    int independentSeats;
+    
+    cout << "How many seats are held by Democrats? ";
+    cin >> democraticSeats;
+    cout << "How many seats are held by Republicans? ";
+    cin >> republicanSeats;
+    cout << "How many seats are held by Independents? ";
+    cin >> independentSeats;
+    
+    int total = republicanSeats + independentSeats;         //took democraticSeats out of the equation
+    
+    double pctRep = 100.0 * republicanSeats / total;
+    double pctDem = 100.0 * democraticSeats / total;
+    double pctInd = 100.0 * independentSeats / total;
+    
+    cout.setf(ios::fixed);       // see pp. 30-31 in Savitch book
+    cout.precision(1);
+    cout << endl << endl;
+    cout << pctRep << "% Republicans" << endl;
+    cout << pctDem << "% Democrats" << endl;
+    cout << pctInd  << "% Independents" << endl;
+    
+    if (pctRep > pctDem)
+    {
+        cout << "Looks like control goes to the Republicans." << endl;
+        
+    }
+    else
+    {
+        cout << "Looks like control goes to the Democrats." << endl;
+        
+    }
+    return(0);
+    
+}
