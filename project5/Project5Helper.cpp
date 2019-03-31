aae461a85bf42ce126251424a8a1bfa23b74cd81
shreya chatterjee <schatterjee22@ucla.edu>
2019-03-30 22:10:53 -0700

    Create Project5Helper.cpp

---
+++ project5/Project5Helper.cpp
@@ -0,0 +1,52 @@
+//
+//  Project5Helper.cpp
+//  Project5Helper
+//
+//  Created by Howard Stahl on 2/13/19.
+//  All rights reserved.
+//
+
+#include "Project5Helper.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+Project5Helper::Project5Helper()
+{
+    
+}
+
+bool Project5Helper::endsWithPM(const string & mainStr )
+{
+    return( endsWith( mainStr, "PM" ) );
+}
+
+bool Project5Helper::endsWith(const string & mainStr, const string & toMatch)
+{
+    bool result( false );
+    if(mainStr.size() >= toMatch.size() &&
+       mainStr.compare(mainStr.size() - toMatch.size(), toMatch.size(), toMatch) == 0)
+    {
+        result = true;
+    }
+    return( result );
+}
+
+
+bool Project5Helper::startsWith(const string & mainStr, const string & toMatch)
+{
+    bool result( false );
+    // std::string::find returns 0 if toMatch is found at starting
+    if(mainStr.find(toMatch) == 0)
+    {
+        result = true;
+    }
+    return( result );
+}
+
+
+bool Project5Helper::startsWithOrchestra(const string & mainStr )
+{
+    return( startsWith( mainStr, "Orchestra" ) );
+}
